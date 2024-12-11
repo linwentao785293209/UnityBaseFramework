092086a526f887325d651239903896b938453a87
linwentao785293209 <785293209@qq.com>
2024-12-11 22:25:58 +0800

    MonoBehaviour管理模块

---
+++ Assets/BaseFramework/Scripts/MonoBehaviour/MonoBehaviourManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine.Events;
+
+namespace BaseFramework
+{
+    /// <summary>
+    /// MonoBehaviour管理器
+    /// </summary>
+    public sealed class ProMonoBehaviourManager : MonoBehaviourAutoSingleton<ProMonoBehaviourManager>
+    {
+        private event UnityAction UpdateEvent;
+        private event UnityAction FixedUpdateEvent;
+        private event UnityAction LateUpdateEvent;
+
+        public void AddUpdateListener(UnityAction updateAction)
+        {
+            UpdateEvent += updateAction;
+        }
+
+        public void RemoveUpdateListener(UnityAction updateAction)
+        {
+            UpdateEvent -= updateAction;
+        }
+
+        public void AddFixedUpdateListener(UnityAction fixedUpdateAction)
+        {
+            FixedUpdateEvent += fixedUpdateAction;
+        }
+
+        public void RemoveFixedUpdateListener(UnityAction fixedUpdateAction)
+        {
+            FixedUpdateEvent -= fixedUpdateAction;
+        }
+
+        public void AddLateUpdateListener(UnityAction lateUpdateAction)
+        {
+            LateUpdateEvent += lateUpdateAction;
+        }
+
+        public void RemoveLateUpdateListener(UnityAction lateUpdateAction)
+        {
+            LateUpdateEvent -= lateUpdateAction;
+        }
+
+        private void Update()
+        {
+            UpdateEvent?.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            FixedUpdateEvent?.Invoke();
+        }
+
+        private void LateUpdate()
+        {
+            LateUpdateEvent?.Invoke();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            UpdateEvent = null;
+            FixedUpdateEvent = null;
+            LateUpdateEvent = null;
+        }
+    }
+}
